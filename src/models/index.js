ba7351ae17d7efed8b66b86290860607c3c67edc
yudhifadilah <115543524+yudhifadilah@users.noreply.github.com>
2026-06-11 19:45:54 +0700

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -6,7 +6,6 @@ const PayoutProduk = require("./PayoutProduk");
 const LimsProduk = require("./LimsProduk");
 const GiftingProduk = require("./giftingProduk");
 
-
 const VilogOrder = require("./VilogOrder");
 const PayoutOrder = require("./PayoutOrder");
 const LimsOrder = require("./LimsOrder");
@@ -20,39 +19,65 @@ const CommunityMember = require("./CommunityMember");
 const StoreSetting = require("./StoreSetting");
 const Tumbal = require("./Tumbal");
 
+/* =========================
+   VILOG
+========================= */
+
 VilogOrder.belongsTo(VilogProduk, {
   foreignKey: "vilogProdukId",
-});
-
-GiftingOrder.belongsTo(GiftingProduk, {
-  foreignKey: "giftingProdukId",
-  as: "produk",
-});
-
-GiftingProduk.hasMany(GiftingOrder, {
-  foreignKey: "giftingProdukId",
-  as: "orders",
+  as: "vilogProduk",
 });
 
 VilogProduk.hasMany(VilogOrder, {
   foreignKey: "vilogProdukId",
+  as: "vilogOrders",
 });
+
+/* =========================
+   PAYOUT
+========================= */
 
 PayoutOrder.belongsTo(PayoutProduk, {
   foreignKey: "payoutProdukId",
+  as: "payoutProduk",
 });
 
 PayoutProduk.hasMany(PayoutOrder, {
   foreignKey: "payoutProdukId",
+  as: "payoutOrders",
 });
+
+/* =========================
+   LIMS
+========================= */
 
 LimsOrder.belongsTo(LimsProduk, {
   foreignKey: "limsProdukId",
+  as: "limsProduk",
 });
 
 LimsProduk.hasMany(LimsOrder, {
   foreignKey: "limsProdukId",
+  as: "limsOrders",
 });
+
+/* =========================
+   GIFTING
+========================= */
+
+GiftingOrder.belongsTo(GiftingProduk, {
+  foreignKey: "giftingProdukId",
+  as: "giftingProduk",
+});
+
+GiftingProduk.hasMany(GiftingOrder, {
+  foreignKey: "giftingProdukId",
+  as: "giftingOrders",
+});
+
+/* =========================
+   REKENING
+========================= */
 
 User.hasMany(RekeningPembayaran, {
   foreignKey: "userId",
@@ -64,30 +89,34 @@ RekeningPembayaran.belongsTo(User, {
   as: "owner",
 });
 
+/* =========================
+   CHAT
+========================= */
+
 ChatRoom.hasMany(ChatMessage, {
   foreignKey: "roomId",
+  as: "messages",
 });
 
 ChatMessage.belongsTo(ChatRoom, {
   foreignKey: "roomId",
+  as: "room",
 });
+
+/* =========================
+   COMMUNITY
+========================= */
 
 RobloxCommunity.hasMany(CommunityMember, {
   foreignKey: "groupId",
   sourceKey: "groupId",
+  as: "members",
 });
 
 CommunityMember.belongsTo(RobloxCommunity, {
   foreignKey: "groupId",
   targetKey: "groupId",
-});
-
-GiftingOrder.belongsTo(GiftingProduk, {
-  foreignKey: "giftingProdukId",
-});
-
-GiftingProduk.hasMany(GiftingOrder, {
-  foreignKey: "giftingProdukId",
+  as: "community",
 });
 
 module.exports = {
@@ -98,12 +127,13 @@ module.exports = {
   VilogProduk,
   PayoutProduk,
   LimsProduk,
+  giftingProduk,
 
   VilogOrder,
   PayoutOrder,
   LimsOrder,
   GiftingOrder,
-  giftingProduk,
+
   RekeningPembayaran,
 
   RobloxCommunity,
